5a8bc60f91fe5ac123b0a2df64fff6a6ad93ae7b
CorinaLucaFocsan <99275268+CorinaLucaFocsan@users.noreply.github.com>
2023-02-05 18:29:02 -0500

    Update PopulationProjector.java

--- src/edu/nyu/cs/PopulationProjector.java
+++ src/edu/nyu/cs/PopulationProjector.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 /**
  * This question is based on Question 1-11 in Liang's Intro to Java textbook.
- * 
- * Complete this program such that it:
- *  - calculates and displays the projected population for each of the next five years.
  *
  * The U.S. Census Bureau projects population based on the following assumptions:
  *  - Current population: 332,403,650
@@ -15,27 +12,14 @@ import java.util.Scanner;
  *  - One death every 13 seconds
  *  - One new immigrant every 45 seconds
  * 
- * Hint:
- *  - There are no fractional people.  All population projections must be integers.
- *  - In Java, if two integers perform division, the result is an integer - the fractional part is truncated.  
- *  - To get an accurate result, one of the values in the division must be a number with a decimal place.
- * 
- * Sample output. Your program should format the output exactly like this: 
- *  Here are the projected population numbers for the next five years:
- *  - Year 2023: 333505890
- *  - Year 2024: 345678825 
- *  - Year 2025: 358766234 
- *  - Year 2026: 365805245 
- *  - Year 2027: 373925136 
  */
 public class PopulationProjector {
 
     /**
      * The main function is automatically called first in a Java program.
-     * Complete the assignment within this function.
      * 
      * @param args An array of any command-line arguments.  Java requires the main function include this, even if not used.
-     * @throws Exception Allows us to not worry about Exceptions in this function. Java requires the main functino include this, even if not used.
+     * @throws Exception.
      */
     public static void main(String[] args) throws Exception {
         
